5a6937f15cb972b7b01f267d8bbf248caf8f4eb7
balramsinghparihar <145901145+balramsinghparihar@users.noreply.github.com>
2024-06-12 17:09:13 +0530

    P4 ifElse value equel on not operator.

--- IfElse/P3_VoterIdValadity.php
+++ IfElse/P3_VoterIdValadity.php
@@ -1,8 +1,19 @@
 <?php
-    $age = 18;
-    if ($age >= 18) {
-        echo "You can vote.";
-    }elseif ($age < 18) {
-        echo "You can't vote.";
-    }
+
+$date1 = new DateTime("2004-11-03");
+
+$date2 = new DateTime("2024-06-09");
+
+$interval = $date1->diff($date2);
+
+echo "difference " . $interval->y . " years, " . $interval->m." months, ".$interval->d." days "; 
+
+// shows the total amount of days (not divided into years, months and days like above)
+// echo "difference " . $interval->days . " days ";
+//     $age = 18;
+//     if ($age >= 18) {
+//         echo "You can vote.";
+//     }elseif ($age < 18) {
+//         echo "You can't vote.";
+//     }
 ?>
---
+++ IfElse/P4_EquelorNot.php
@@ -0,0 +1,9 @@
+<?php
+    $a = 12;
+    $b = "12";
+    if ($a === $b) {
+        echo "Condition is true.";
+    }else {
+        echo "Condition is false.";
+    }
+?>
